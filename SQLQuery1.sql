8ea22c906202807e64532c973da70cce131e1368
Jim Castrigano II <castriganoj@gmail.com>
2019-05-23 12:48:55 +0000

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT sOrgPath, sName, a.*
-FROM tracer.aud_correctiveaction a
-INNER JOIN tracer.aud_finding f ON a.IAuditFileID = f.IAuditFileID  AND a.ICorrActionSN = f.IFindingID 
-INNER JOIN tracer.AuditFile ad ON f.IAuditFileID = ad.IAuditFileID
-INNER JOIN tracer.Status s ON s.IStatusID = a.sStatus
-
